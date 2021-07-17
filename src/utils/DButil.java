693d6b79ab90612b30a38fccce7e9910a0de97d6
maskotto <maskotto@master>
2021-07-17 13:36:56 +0900

    データベース関連のファイルの追加

---
+++ src/utils/DButil.java
@@ -0,0 +1,20 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DButil {
+    private static final String PERSISTENCE_UNIT_NAME = "techacademy_orm";//使う時はここだけ変更する
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if(emf==null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return emf;
+    }
+}
